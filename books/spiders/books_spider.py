4d89f49a7df0f80c74314ad90a5337a0d1c76b03
Ivan Tolstov <everthinq@Ivans-MacBook-Air.local>
2023-06-01 19:12:21 +0300

    books_spider update

--- books/spiders/books_spider.py
+++ books/spiders/books_spider.py
@@ -21,16 +21,18 @@ class BooksSpider(scrapy.Spider):
         book = response.css("div.product_main")[0]
         table_rows = response.css("table tr")
         yield {
-            "href": response.request.url,
-            "title": response.css("h1::text").get(),
+            "url": response.url,
+            "title": book.css("h1::text").get(),
+            "price": book.css("p.price_color::text").get(),
+            "rating": book.css("p.star-rating").attrib['class'],
             "category": response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
-            "price": response.css("p.price_color::text").get(),
-            "upc": response.css("table.table-striped tr > td::text")[0].get(),
-            "product_type": response.css("table.table-striped tr > td::text")[1].get(),
-            "tax": response.css("table.table-striped tr > td::text")[4].get(),
-            "availability": response.css("table.table-striped tr > td::text")[5].get(),
-            "number_of_reviews": response.css("table.table-striped tr > td::text")[6].get(),
-            "rating": response.css("p.star-rating").attrib['class'],
+            "upc": table_rows[0].css("td::text").get(),
+            "product_type": table_rows[1].css("td::text").get(),
+            "price_exl_tax": table_rows[2].css("td::text").get(),
+            "price_incl_tax": table_rows[3].css("td::text").get(),
+            "tax": table_rows[4].css("td::text").get(),
+            "availability": table_rows[5].css("td::text").get(),
+            "number_of_reviews": table_rows[6].css("td::text").get(),
             "description": response.css("#product_description ~ p::text").get(),
             "test": response.meta["test"]
         }
